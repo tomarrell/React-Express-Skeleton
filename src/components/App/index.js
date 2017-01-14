925590293155d1d39a2a8e052a0f0209e7f22df9
Tom Arrell <tom.arrell@gmail.com>
2017-01-14 18:44:00 +1300

    Updated start screen info

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -10,9 +10,16 @@ function App() {
         {/* <img src={logo} className={style.appLogo} alt="logo" /> */}
         <h2>Tom React-Express Skeleton</h2>
       </div>
-      <p className={style.appIntro}>
-        To get started, edit <code>src/components/App.js</code> and save to reload.
-      </p>
+      <div className={style.appIntro}>
+        <h5>Adding React</h5>
+        <p>
+          To get started, edit <code>src/components/App.js</code> and save to reload.
+        </p>
+        <h5>Node Server</h5>
+        <p>
+          To start the node server run the command <code>yarn server</code>.
+        </p>
+      </div>
     </div>
   );
 }
