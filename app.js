b7530a5e2407e74d43906937d8985b2819088983
Tom Arrell <tom.arrell@gmail.com>
2017-01-16 21:20:09 +1300

    Plurals

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 // Serve static assets
 app.use(express.static(path.resolve(__dirname, 'build')));
 
-// Always return the main index.html, so react-router render the route in the client
+// Always return the main index.html, so react-router renders the route in the client
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
